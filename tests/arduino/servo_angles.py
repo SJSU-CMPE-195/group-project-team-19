311ae7d09d9fd9fa60208c4aabaf678d95dc57f0
Philip Figuerres <pfiguerres@gmail.com>
2026-03-16 19:12:27 -0700

    added servo angle helper function

--- tests/arduino/servo_angles.py
+++ tests/arduino/servo_angles.py
@@ -5,3 +5,9 @@ from time import sleep
 #board initialization and confirmation print statement
 board = Arduino('/dev/cu.usbmodem101')
 print("[INFO] Arduino connected, firmware version:", board.firmware)
+
+
+def get_servo_angle() -> int:
+    """Recieve an angle from the user to be used for the servo."""
+    print("Input angle")
+    return int(input())
