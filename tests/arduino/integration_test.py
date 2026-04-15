03bb61891847ad42de8ec5713b6e263e593164d6
Phil <pfiguerres@gmail.com>
2026-04-15 15:59:44 -0700

    test single servo when inputting command

--- tests/arduino/integration_test.py
+++ tests/arduino/integration_test.py
@@ -1,9 +1,13 @@
 import servo_controller as sc
+import time
+
 
 def main():
     print("Integration Test")
-    #servo test
+    #servo test existence and home position
     test_setup()
+    #servo test movement one at a time
+    test_single()
 
 if __name__ == "__main__":
     main()
@@ -14,3 +18,20 @@ def test_setup():
         for name in sc.servos:
             #verify home position and that it is recongnized
             print(name, sc.servos[name].current_angle)
+
+#testing servo movement
+def test_single():
+    print("Testing 1 servo at a time")
+
+    for i in range(6):
+        #servo starting from bottom s0-s5
+        name = "s" + str(i)
+        #test angle
+        angle = 45
+
+        #send command through controller 
+        sc.servos[name].set_angle(angle)
+        time.sleep(0.3)
+
+        #print updated angle, after movement
+        print(name, "->", sc.servos[name].current_angle)
