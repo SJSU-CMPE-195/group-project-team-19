b59a444896fe7217b9a5c055ae8de9b1fba08df8
Phil <pfiguerres@gmail.com>
2026-04-15 16:03:30 -0700

    add pass fail check for servo movement

--- tests/arduino/integration_test.py
+++ tests/arduino/integration_test.py
@@ -23,6 +23,8 @@ def test_setup():
 def test_single():
     print("Testing 1 servo at a time")
 
+    ok = True
+
     for i in range(6):
         #servo starting from bottom s0-s5
         name = "s" + str(i)
@@ -34,4 +36,14 @@ def test_single():
         time.sleep(0.3)
 
         #print updated angle, after movement
-        print(name, "->", sc.servos[name].current_angle)
+        #print(name, "->", sc.servos[name].current_angle)
+
+        #adding pass fail for maintability and testing
+        if sc.servos[name].current_angle == angle:
+            #print the servo name and pass/fail
+            print(name, "passed")
+        else:
+            print(name, "failed")
+            ok = False
+            
+    return ok
