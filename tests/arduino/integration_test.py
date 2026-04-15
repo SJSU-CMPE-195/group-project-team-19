9e30691ca280d3acc301341429b5a898f7f75aec
Phil <pfiguerres@gmail.com>
2026-04-15 15:53:45 -0700

    add setup test to check servo initialization

--- tests/arduino/integration_test.py
+++ tests/arduino/integration_test.py
@@ -2,6 +2,15 @@ import servo_controller as sc
 
 def main():
     print("Integration Test")
+    #servo test
+    test_setup()
 
 if __name__ == "__main__":
     main()
+
+def test_setup():
+        print("Testing setup for servos")
+
+        for name in sc.servos:
+            #verify home position and that it is recongnized
+            print(name, sc.servos[name].current_angle)
